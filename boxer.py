5b61ab00b2bd1c82b04a89845802cb40b950df15
Hugo Drakeskär <hugodrakeskar@gmail.com>
2021-10-24 03:27:13 +0200

    fourther add to boxes

--- boxer.py
+++ boxer.py
@@ -18,12 +18,15 @@ class Boxer:
     def __init__(self):
         self.width, self.height = get_terminal_size()
 
-    def form_box(self, width, height, data):
+    def form_box(self, width, height, data=None):
         matrix = []
         x1, y1 = 0, 0
         x2, y2 = width-1, height-1
+        data_i = 0
         for ri in range(height):
             row = []
+            if data and type(data) == list:
+                data_i = 0
             for ci in range(width):
                 char = "E"
                 if ri == y1:
@@ -43,8 +46,15 @@ class Boxer:
                         char = SYMBOLS["VertLine"]
                 elif ci == x1 or ci == x2:
                     char = SYMBOLS["HorLine"]
-                elif ci == x1 + 1 and ri == y1 + 1:
-                    char = data
+                elif data:
+                    if type(data) == str and data_i < len(data):
+                        char = data[data_i]
+                        data_i += 1
+                    elif type(data) == list and ri <= len(data) and data_i < len(data[ri-1]):
+                        char = data[ri-1][data_i]
+                        data_i += 1
+                    else:
+                        char = SYMBOLS["Blank"]
                 else:
                     char = SYMBOLS["Blank"]
 
@@ -53,25 +63,34 @@ class Boxer:
             matrix.append(row)
         return matrix
 
-    def form_grid(self, w, h, count):
+    def form_grid(self, w, h, count, data):
         matrix = []
         x1, y1 = 0, 0
         x2, y2 = self.width-1, self.height-1
-        pad = 0
+        pad = w
         cols = x2 // (w+pad)
         rows = count // cols + 1
-
-        temp_data = [0 for x in range(count)]
-
-        box_list = [self.form_box(w, h, str(bi)) for bi in range(count)]
+        if data:
+            #box_list = [self.form_box(w, h, data[bi]) for bi in range(count)]
+            box_list = []
+            for bi in range(count):
+                if bi < len(data):
+                    content = data[bi]
+                else:
+                    content = None
+                box_list.append(self.form_box(w, h, content))
+        else:
+            box_list = [self.form_box(w, h) for bi in range(count)]
         start = 0
         end = cols
         for ri in range(rows):
             for br in range(h):
                 row = []
                 for box in box_list[start:end]:
-                    row.extend(box[br])
+                    row.extend(box[br]+[" "*pad])
                 matrix.append(row)
+                # pad_row = [" "*len(matrix[-1])]
+                # matrix.append(pad_row)
             start = end
             end += cols
             if end > count:
@@ -79,8 +98,8 @@ class Boxer:
 
         return matrix
 
-    def draw_grid(self, w, h, count, color=None):
-        grid = self.form_grid(w, h, count)
+    def draw_grid(self, w, h, count, data=None, color=None):
+        grid = self.form_grid(w, h, count, data)
         self.draw(grid, color)
 
     def draw_box(self, width, height, color=None):
@@ -108,4 +127,4 @@ B = Boxer()
 #B.draw_box(150,30, "BLUE")
 # time.sleep(2)
 # B.clear()
-B.draw_grid(30, 7, 10)
+B.draw_grid(10, 7, 20, ["AaaaaaaaaaaaaaaaaaaaAaaaaaaaaaaaaaaaaaaAAA", ["abc", "def", "Aaaaaaaaaaaaaaaaaaaa"], "C", "D"])
